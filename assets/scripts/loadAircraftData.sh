bab43d3107d7182dc1d5e846aca3edd4f3a28f04
RexKramer1 <74321408+RexKramer1@users.noreply.github.com>
2023-09-24 18:52:54 +0200

    Improve formatting

--- assets/scripts/loadAircraftData.sh
+++ assets/scripts/loadAircraftData.sh
@@ -47,11 +47,8 @@ psql -c "COPY TMP_AIRCRAFT_DATA (HEX,
 							ACARS,
 							NOTES,
 							CATEGORY_DESCRIPTION)
-FROM '$pathToDirectoryWithCsv/aircraftDatabase.csv' WITH
-DELIMITER ',' CSV HEADER;" -U beluga -d belugaDb
-
-psql -c "\echo number of imported records in temp-file 1:" -U beluga -d belugaDb
-psql -c "SELECT COUNT(*) FROM TMP_AIRCRAFT_DATA;" -U beluga -d belugaDb
+		FROM '$pathToDirectoryWithCsv/aircraftDatabase.csv' WITH
+			DELIMITER ',' CSV HEADER;" -U beluga -d belugaDb
 
 echo ------------------------------------------------------------
 echo copy to temp-file 2 with additional column hex_low
@@ -60,68 +57,64 @@ echo ------------------------------------------------------------
 psql -c "DROP TABLE IF EXISTS TMP_AIRCRAFT_DATA2;" -U beluga -d belugaDb
 
 psql -c "CREATE TABLE TMP_AIRCRAFT_DATA2 (HEX CHARACTER VARYING(255) NOT NULL,
-													HEX_LOW CHARACTER VARYING(255) NOT NULL,
-													ACARS CHARACTER VARYING(255),
-													ADSB CHARACTER VARYING(255),
-													BUILT CHARACTER VARYING(255),
-													CATEGORY_DESCRIPTION CHARACTER VARYING(255),
-													ENGINES CHARACTER VARYING(255),
-													FIRST_FLIGHT_DATE CHARACTER VARYING(255),
-													ICAO_AIRCRAFT_TYPE CHARACTER VARYING(255),
-													LINE_NUMBER CHARACTER VARYING(255),
-													MANUFACTURER_ICAO CHARACTER VARYING(255),
-													MANUFACTURER_NAME CHARACTER VARYING(255),
-													MODEL CHARACTER VARYING(255),
-													MODES CHARACTER VARYING(255),
-													NOTES CHARACTER VARYING(255),
-													OPERATOR_CALLSIGN CHARACTER VARYING(255),
-													OPERATOR_IATA CHARACTER VARYING(255),
-													OPERATOR_ICAO CHARACTER VARYING(255),
-													OPERATOR_NAME CHARACTER VARYING(255),
-													OWNER_NAME CHARACTER VARYING(255),
-													REG_UNTIL CHARACTER VARYING(255),
-													REGISTERED CHARACTER VARYING(255),
-													REGISTRATION CHARACTER VARYING(255),
-													SEAT_CONFIGURATION CHARACTER VARYING(255),
-													SERIAL_NUMBER CHARACTER VARYING(255),
-													STATUS CHARACTER VARYING(255),
-													TEST_REG CHARACTER VARYING(255),
-													TYPECODE CHARACTER VARYING(255));" -U beluga -d belugaDb
+							HEX_LOW CHARACTER VARYING(255) NOT NULL,
+							ACARS CHARACTER VARYING(255),
+							ADSB CHARACTER VARYING(255),
+							BUILT CHARACTER VARYING(255),
+							CATEGORY_DESCRIPTION CHARACTER VARYING(255),
+							ENGINES CHARACTER VARYING(255),
+							FIRST_FLIGHT_DATE CHARACTER VARYING(255),
+							ICAO_AIRCRAFT_TYPE CHARACTER VARYING(255),
+							LINE_NUMBER CHARACTER VARYING(255),
+							MANUFACTURER_ICAO CHARACTER VARYING(255),
+							MANUFACTURER_NAME CHARACTER VARYING(255),
+							MODEL CHARACTER VARYING(255),
+							MODES CHARACTER VARYING(255),
+							NOTES CHARACTER VARYING(255),
+							OPERATOR_CALLSIGN CHARACTER VARYING(255),
+							OPERATOR_IATA CHARACTER VARYING(255),
+							OPERATOR_ICAO CHARACTER VARYING(255),
+							OPERATOR_NAME CHARACTER VARYING(255),
+							OWNER_NAME CHARACTER VARYING(255),
+							REG_UNTIL CHARACTER VARYING(255),
+							REGISTERED CHARACTER VARYING(255),
+							REGISTRATION CHARACTER VARYING(255),
+							SEAT_CONFIGURATION CHARACTER VARYING(255),
+							SERIAL_NUMBER CHARACTER VARYING(255),
+							STATUS CHARACTER VARYING(255),
+							TEST_REG CHARACTER VARYING(255),
+							TYPECODE CHARACTER VARYING(255));" -U beluga -d belugaDb
 
 psql -c "INSERT INTO TMP_AIRCRAFT_DATA2
-SELECT HEX,
-	lower(HEX),
-	ACARS,
-	ADSB,
-	BUILT,
-	CATEGORY_DESCRIPTION,
-	ENGINES,
-	FIRST_FLIGHT_DATE,
-	ICAO_AIRCRAFT_TYPE,
-	LINE_NUMBER,
-	MANUFACTURER_ICAO,
-	MANUFACTURER_NAME,
-	MODEL,
-	MODES,
-	NOTES,
-	OPERATOR_CALLSIGN,
-	OPERATOR_IATA,
-	OPERATOR_ICAO,
-	OPERATOR_NAME,
-	OWNER_NAME,
-	REG_UNTIL,
-	REGISTERED,
-	REGISTRATION,
-	SEAT_CONFIGURATION,
-	SERIAL_NUMBER,
-	STATUS,
-	TEST_REG,
-	TYPECODE
-FROM TMP_AIRCRAFT_DATA;" -U beluga -d belugaDb
-
-psql -c "\echo number of imported records in temp-file 2:" -U beluga -d belugaDb
-
-psql -c "SELECT COUNT(*) FROM TMP_AIRCRAFT_DATA2;" -U beluga -d belugaDb
+			SELECT HEX,
+				lower(HEX),
+				ACARS,
+				ADSB,
+				BUILT,
+				CATEGORY_DESCRIPTION,
+				ENGINES,
+				FIRST_FLIGHT_DATE,
+				ICAO_AIRCRAFT_TYPE,
+				LINE_NUMBER,
+				MANUFACTURER_ICAO,
+				MANUFACTURER_NAME,
+				MODEL,
+				MODES,
+				NOTES,
+				OPERATOR_CALLSIGN,
+				OPERATOR_IATA,
+				OPERATOR_ICAO,
+				OPERATOR_NAME,
+				OWNER_NAME,
+				REG_UNTIL,
+				REGISTERED,
+				REGISTRATION,
+				SEAT_CONFIGURATION,
+				SERIAL_NUMBER,
+				STATUS,
+				TEST_REG,
+				TYPECODE
+			FROM TMP_AIRCRAFT_DATA;" -U beluga -d belugaDb
 
 echo ------------------------------------------------------------
 echo copy to destination table aircraft_data
@@ -161,45 +154,42 @@ psql -c "INSERT INTO AIRCRAFT_DATA(
 				TEST_REG,
 				TYPECODE	
 			)
-SELECT DISTINCT ON (HEX_LOW) 
-HEX,
-	ACARS,
-	ADSB,
-	BUILT,
-	CATEGORY_DESCRIPTION,
-	ENGINES,
-	FIRST_FLIGHT_DATE,
-	ICAO_AIRCRAFT_TYPE,
-	LINE_NUMBER,
-	MANUFACTURER_ICAO,
-	MANUFACTURER_NAME,
-	MODEL,
-	MODES,
-	NOTES,
-	OPERATOR_CALLSIGN,
-	OPERATOR_IATA,
-	OPERATOR_ICAO,
-	OPERATOR_NAME,
-	OWNER_NAME,
-	REG_UNTIL,
-	REGISTERED,
-	REGISTRATION,
-	SEAT_CONFIGURATION,
-	SERIAL_NUMBER,
-	STATUS,
-	TEST_REG,
-	TYPECODE
-FROM TMP_AIRCRAFT_DATA2
-ORDER BY HEX_LOW, HEX;" -U beluga -d belugaDb
+			SELECT DISTINCT ON (HEX_LOW) 
+			HEX,
+				ACARS,
+				ADSB,
+				BUILT,
+				CATEGORY_DESCRIPTION,
+				ENGINES,
+				FIRST_FLIGHT_DATE,
+				ICAO_AIRCRAFT_TYPE,
+				LINE_NUMBER,
+				MANUFACTURER_ICAO,
+				MANUFACTURER_NAME,
+				MODEL,
+				MODES,
+				NOTES,
+				OPERATOR_CALLSIGN,
+				OPERATOR_IATA,
+				OPERATOR_ICAO,
+				OPERATOR_NAME,
+				OWNER_NAME,
+				REG_UNTIL,
+				REGISTERED,
+				REGISTRATION,
+				SEAT_CONFIGURATION,
+				SERIAL_NUMBER,
+				STATUS,
+				TEST_REG,
+				TYPECODE
+			FROM TMP_AIRCRAFT_DATA2
+			ORDER BY HEX_LOW, HEX;" -U beluga -d belugaDb
 
 echo -----------------------------------------
 echo update data source in aircraft_data
 echo -----------------------------------------
 psql -c "UPDATE AIRCRAFT_DATA
 			SET data_source = 'OpenSkyNetwork';" -U beluga -d belugaDb
-
-psql -c "\echo number of distinct records in table aircraft_data:" -U beluga -d belugaDb
-psql -c "SELECT COUNT(*) FROM AIRCRAFT_DATA;" -U beluga -d belugaDb
 
 echo -----------------------------------------
 echo cleanup ... drop temp tables
@@ -227,9 +217,6 @@ psql -c "COPY tmp_aircraft_data_mictronics (
 			special_tag)
 		FROM '$pathToDirectoryWithCsv/aircrafts.csv'
 		WITH DELIMITER ',' CSV HEADER;" -U beluga -d belugaDb
-
-psql -c "\echo number of imported records in TMP_AIRCRAFT_DATA_MICTRONICS:" -U beluga -d belugaDb
-psql -c "SELECT COUNT(*) FROM TMP_AIRCRAFT_DATA_MICTRONICS;" -U beluga -d belugaDb
 
 echo -----------------------------------------
 echo load types data from mictronics
