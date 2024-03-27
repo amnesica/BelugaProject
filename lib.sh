73c23ed8a1cc5849a0333608daca56fee98baaaf
amnesica <54629966+amnesica@users.noreply.github.com>
2024-03-27 14:51:35 +0100

    Edit lib.sh

--- lib.sh
+++ lib.sh
@@ -160,6 +160,8 @@ _docker_rm_container() {
   if [[ $# -eq 0 ]]; then
     _ask_user_with_message "Do you really want to remove all containers for the Beluga Project (y/n)?"
     _docker_rm_all_containers
+  elif [[ "$1" == "update"]]; then
+    _docker_rm_all_containers
   else
     echo "Remove container $1 ..."
     local container_id=$(docker ps -aqf "name=$1")
