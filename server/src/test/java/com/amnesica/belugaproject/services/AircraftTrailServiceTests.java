2e27f9d287fa071ae4855556ef04b47fa0b91b06
amnesica <54629966+amnesica@users.noreply.github.com>
2024-11-03 11:59:10 +0100

    Add profile to test class

--- server/src/test/java/com/amnesica/belugaproject/services/AircraftTrailServiceTests.java
+++ server/src/test/java/com/amnesica/belugaproject/services/AircraftTrailServiceTests.java
@@ -12,12 +12,14 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
 @SpringBootTest(classes = {AircraftTrailService.class})
 @ExtendWith(MockitoExtension.class)
 public class AircraftTrailServiceTests {
